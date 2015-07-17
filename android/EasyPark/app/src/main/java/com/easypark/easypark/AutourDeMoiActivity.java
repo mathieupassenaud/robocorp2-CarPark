73732ad57ce2b99dd56e0c2a3767f709d004a4f9
loic <labarrereloic@gmail.com>
2015-07-17 13:40:32 +0100

    Refonte propre structure IHM

---
+++ android/EasyPark/app/src/main/java/com/easypark/easypark/AutourDeMoiActivity.java
@@ -0,0 +1,52 @@
+package com.easypark.easypark;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class AutourDeMoiActivity extends ActionBarActivity {
+
+    Button butPlusProche;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autour_de_moi);
+
+
+        butPlusProche = (Button)findViewById(R.id.butAutourMoi);
+        butPlusProche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_autour_de_moi, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
